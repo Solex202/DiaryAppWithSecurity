78c9f3afd386155b78ddd4b1c2318c745f3ab2b5
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-13 09:53:24 +0100

    my security app

---
+++ src/main/java/com/technophiles/Diaryapppro/security/UnauthorizedEntryPoint.java
@@ -0,0 +1,22 @@
+package com.technophiles.Diaryapppro.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+@Component
+@Slf4j
+public class UnauthorizedEntryPoint implements AuthenticationEntryPoint, Serializable {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.info("unauthorized entry info ---> ", authException);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
