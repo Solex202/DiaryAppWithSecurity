c0925cf654f3820ac5b4b59d2ab64aa78a6e5b88
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-13 12:28:25 +0100

    my security app

---
+++ src/main/java/com/technophiles/Diaryapppro/controllers/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.technophiles.Diaryapppro.controllers.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String email;
+    private String password;
+}
