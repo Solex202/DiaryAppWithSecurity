252c0623366f27ccc091bebca1bb3ae85b892952
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-13 16:40:05 +0100

    my security app

---
+++ src/main/java/com/technophiles/Diaryapppro/security/jwt/SecurityConstant.java
@@ -0,0 +1,6 @@
+package com.technophiles.Diaryapppro.security.jwt;
+
+public class SecurityConstant {
+    final static String SIGNING_KEY = "SECRET_KEY";
+    final static String AUTHORITIES_KEY = "ROLE_";
+}
